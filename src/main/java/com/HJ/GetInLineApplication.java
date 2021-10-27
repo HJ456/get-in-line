7151ca681295595ba0ea961b0ad50f24716a9d3f
hj <gmdwp789@naver.com>
2021-10-27 12:39:50 +0900

    초기 세팅

---
+++ src/main/java/com/HJ/GetInLineApplication.java
@@ -0,0 +1,12 @@
+package com.HJ;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class GetInLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetInLineApplication.class, args);
+    }
+}
---
+++ src/main/java/com/HJ/controller/EventController.java
@@ -0,0 +1,22 @@
+package com.HJ.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/events")
+@Controller
+public class EventController {
+
+    @GetMapping("/")
+    public String events(){
+        return "event/index";
+    }
+
+    @GetMapping("/{eventid}")
+    public String eventDetail(@PathVariable Integer eventId) {
+        return "event/detail";
+    }
+
+}
